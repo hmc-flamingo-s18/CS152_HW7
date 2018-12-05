fdeae422f95b4eff8c6b7b5e77305f11e5b56a3d
Flamingo <wli@cs.hmc.edu>
2018-12-05 05:11:52 -0800

    2

--- CS152_7.py
+++ CS152_7.py
@@ -1,9 +1,9 @@
 import random
+import numpy as np
 
 random.seed()
 
-episodesPerLoop = 2
-LOOPS = 5
+EPISODES = 10
 
 arrows = ["^", ">", "v", "<"]
 returns = [[[], [], [], []] for i in range(9)]
@@ -19,44 +19,56 @@ next_states = [[0, 1, 3, 0],
                [4, 8, 7, 6],
                [5, 8, 8, 7]]
 
-for i in range(LOOPS):        
-    for j in range(episodesPerLoop):
-        s = random.randrange(0, 8)
-        intended_a = random.randrange(0, 4)
-        r = 0
-        rewards = []
-        while s != 8:
-            actual_a = intended_a
-            prob1 = random.randrange(0, 10)
-            if  prob1 == 0:
-                actual_a = (intended_a + 1) % 4
-            elif prob1 == 9:
-                actual_a = (intended_a - 1) % 4
+for i in range(EPISODES):        
+    print(f"trace_{i}")
+    s = random.randrange(0, 8)
+    intended_a = random.randrange(0, 4)
+    r = 0
+    rewards = []
+    while s != 8:
+        actual_a = intended_a
+        prob1 = random.randrange(0, 10)
+        if  prob1 == 0:
+            actual_a = (intended_a + 1) % 4
+        elif prob1 == 9:
+            actual_a = (intended_a - 1) % 4
 
-            trace = f"(r={r}, s={s}, {arrows[intended_a]}, {arrows[actual_a]}"
-            print(trace)
+        trace = f"(r={r}, s={s}, {arrows[intended_a]}, {arrows[actual_a]}"
+        print(trace)
+        
+        if s == next_states[s][actual_a]:
+            r = -10
+        else:
+            r = -1
+        rewards.append((s, intended_a, r))       
+        s = next_states[s][actual_a]        
+        prob2 = random.randrange(0, 100)
+        if prob2 < policy[s][0]:
+            intended_a = 0
+        elif prob2 < policy[s][0] + policy[s][1]:
+            intended_a = 1
+        elif prob2 < policy[s][0] + policy[s][1] + policy[s][2]:
+            intended_a = 2
+        else:
+            intended_a = 3
+    for index, reward in enumerate(rewards):
+        for k in rewards[index:]:
+            if q[reward[0]][reward[1]] == -9999:
+                q[reward[0]][reward[1]] = k[2]
+            else:
+                q[reward[0]][reward[1]] += k[2]
+    returns.append(np.array(q))
+
+    averages = numpy.array([])
+    for q in returns:
+        averages += q
+    averages = averages / len(returns)
+
+    for l in range(len(policy)):
+        policy[l] = [0, 0, 0, 0]
+        policy[l][np.argmax(averages[l])] = 100
+        
             
-            if s == next_states[s][actual_a]:
-                r = -10
-            else:
-                r = -1
-            rewards.append((s, intended_a, r))       
-            s = next_states[s][actual_a]        
-            prob2 = random.randrange(0, 100)
-            if prob2 < policy[s][0]:
-                intended_a = 0
-            elif prob2 < policy[s][0] + policy[s][1]:
-                intended_a = 1
-            elif prob2 < policy[s][0] + policy[s][1] + policy[s][2]:
-                intended_a = 2
-            else:
-                intended_a = 3
-        for index, reward in enumerate(rewards):
-            for k in rewards[index:]:
-                if q[reward[0]][reward[1]] == -9999:
-                    q[reward[0]][reward[1]] = k[2]
-                else:
-                    q[reward[0]][reward[1]] += k[2]
              
             
 
